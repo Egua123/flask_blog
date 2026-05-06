9c053d74df1b07224458c0422e17ee555d08f813
marioDektop <mariosekpona@gmail.com>
2026-05-06 10:33:14 -0400

    Ajout de post et de la pagination du blog.

--- run.py
+++ run.py
@@ -1,4 +1,9 @@
-from flaskblog import app
+from flaskblog import app, db
+from flaskblog.models import User, Post
+
+
+with app.app_context():
+    db.create_all()
 
 if __name__ == "__main__":
     app.run(debug=True, use_reloader=True)
